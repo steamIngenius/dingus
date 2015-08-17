e4200eabd4a8146d29047609dabe70bfe2186267
Micah Hawkins <micahbhawkins@gmail.com>
2015-08-16 23:53:24 -0700

    user wants to move around

--- HelloDungeon.java
+++ HelloDungeon.java
@@ -1,6 +1,7 @@
 import net.slashie.libjcsi.wswing.WSwingConsoleInterface;
 import net.slashie.libjcsi.ConsoleSystemInterface;
 import net.slashie.libjcsi.CSIColor;
+import net.slashie.libjcsi.CharKey;
 import java.util.Properties;
 
 public class HelloDungeon {
@@ -19,7 +20,34 @@ public class HelloDungeon {
 			System.exit(-1);
 		}
 
-		csi.print(0,0, "Hello! Welcome to Dingus!", CSIColor.WHITE);
+		csi.print(0,0, "Hello! Welcome to Dingus!", CSIColor.COSMIC_LATTE);
 		csi.refresh();
+
+		int x = 0;
+		int y = 0;
+		boolean stop = false;
+
+		while(!stop) {
+			csi.cls();
+			csi.print(x,y, "@", CSIColor.WHITE);
+			csi.refresh();
+			CharKey dir = csi.inkey();
+
+			if (dir.isUpArrow()&& (y-1 >= 0)) {
+				y--;
+			}
+			if (dir.isDownArrow()&& (y+1 < 25)) {
+				y++;
+			}
+			if (dir.isLeftArrow()&& (x-1 >= 0)) {
+				x--;
+			}
+			if (dir.isRightArrow()&& (x+1 < 80)) {
+				x++;
+			}
+			if (dir.code == CharKey.Q) {
+				stop = true;
+			}
+		}
 	}
 }
